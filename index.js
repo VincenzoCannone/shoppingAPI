9acd67cccfd8eb7838ee119df9a506d70937eba4
enzo <cannone.vin@gmail.com>
2022-01-19 14:31:25 +0100

    filter adjustments

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
 // const stripeRoute = require("./routes/stripe");
-// const cors = require("cors");
+const cors = require("cors");
 
 dotenv.config();
 
@@ -19,7 +19,7 @@ mongoose
     console.log(err);
   });
 
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
@@ -28,6 +28,7 @@ app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
 // app.use("/api/checkout", stripeRoute);
 
-app.listen(process.env.PORT || 5000, () => {
-  console.log("Backend server is running!");
+const portNumber = process.env.PORT;
+app.listen(portNumber || 5000, () => {
+  console.log(`Backend server is running! on port number ` + portNumber);
 });
